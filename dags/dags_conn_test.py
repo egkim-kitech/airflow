257e05904cb5f65173d7c0890baa6b9112605b44
egkim <egkim@kitech.re.kr>
2024-12-10 18:33:55 +0900

    dags_conn_test added

---
+++ dags/dags_conn_test.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_conn_test",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
+    # make empthy operator
+    t1 = EmptyOperator(task_id="t1")
+    t2 = EmptyOperator(task_id="t2")
+
+    # make EmptyOperator t3 to t8
+    t3 = EmptyOperator(task_id="t3")
+    t4 = EmptyOperator(task_id="t4")
+    t5 = EmptyOperator(task_id="t5")
+    t6 = EmptyOperator(task_id="t6")
+    t7 = EmptyOperator(task_id="t7")
+    t8 = EmptyOperator(task_id="t8")
+    
+    t1 >> [t2,t3] >> t4
+    t5 >> t4
+    [t4,t7] >>t6 >> t8 
